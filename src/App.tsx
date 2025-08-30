0eb85e2c3901fcf55f190d097f2692b00060432a
mxte-b <182339124+mxte-b@users.noreply.github.com>
2025-08-30 17:58:06 +0200

    Basic file structure

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './assets/styles/App.css'
+import Portfolio from './assets/pages/Portfolio'
+
+function App() {
+  return (
+    <Portfolio />
+  )
+}
+
+export default App
---
+++ src/assets/pages/Portfolio.tsx
@@ -0,0 +1,12 @@
+const Portfolio = () => {
+    return (
+        <>
+            <h1>Portfolio</h1>
+            <p>Hi, I'm mxte_b!</p>
+            <p>A generalist developer</p>
+            <span>With more than 5 years of programming experience</span>
+        </>
+    );
+};
+
+export default Portfolio;
