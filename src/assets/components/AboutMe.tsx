28ad7b675bcaf36d9b9a0bde6c43564d480344e0
mxte-b <182339124+mxte-b@users.noreply.github.com>
2026-02-15 15:50:30 +0100

    About me

--- src/assets/components/AboutMe.tsx
+++ src/assets/components/AboutMe.tsx
@@ -1,3 +1,5 @@
+import Icons from "./Icons";
+
 const AboutMe = () => {
     return (
         <section className="about-me" id="about-me">
@@ -5,7 +7,53 @@ const AboutMe = () => {
                 <div className="idx">1.</div>
                 <div className="title">About Me</div>
             </header>
-            <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe recusandae unde necessitatibus non doloribus eum vero quibusdam amet exercitationem consequuntur nam quas distinctio libero nihil ad culpa, cumque ea sit! Porro adipisci, perferendis sint eum quas reiciendis dolores repellat quisquam deserunt quia labore. Quae qui ratione quia! Possimus eius labore, cumque minus perferendis quae nemo modi libero voluptatem in ad mollitia alias, et voluptas tenetur sequi, corrupti odit quaerat reiciendis perspiciatis ipsum quia sed! Dolore iste exercitationem minus placeat soluta. Quidem totam aspernatur quam quisquam officiis optio recusandae temporibus, dignissimos alias pariatur ratione iure! Illo quidem suscipit at dolore, iusto consequuntur aspernatur culpa nesciunt, velit qui ratione blanditiis.</div>
+            <div className="content">
+                <div className="intro">
+                    <h3>My name is <i>Norman M. Blank</i>, known online as <i>mxte_b</i>.</h3>
+                    <p>I am 20 years old, and I enjoy learning and expanding my knowledge in every way I can.</p>
+                    <p>I am especially interested in Fractal Rendering, Cryptography and Web Development, and have made numerous projects related to these areas.</p>
+                    <p>Alongside my projects, I have consistently achieved outstanding academic performance for over 14 years.</p>
+                </div>
+                
+                <div className="events">
+                    <div className="events__header">
+                        <Icons.MortarboardFill className="events__icon" />
+                        <h3 className="events__title">Education</h3>
+                    </div>
+                    <div className="events__items">
+                        <div className="event-item">
+                            <div className="event-item__body">
+                                <h4 className="event-item__title">Computer Science BSc</h4>
+                                <div className="event-item__description">University of Pannonia</div>
+                            </div>
+                            <span className="event-item__timespan">2025 - Present</span>
+                        </div>
+                        <div className="event-item">
+                            <div className="event-item__body">
+                                <h4 className="event-item__title">Software Development & Testing - Technical Diploma</h4>
+                                <div className="event-item__description">VSZC Ipari Technical Secondary School</div>
+                            </div>
+                            <span className="event-item__timespan">2020 - 2025</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="events">
+                    <div className="events__header">
+                        <Icons.AwardFill className="events__icon" />
+                        <h3 className="events__title">Awards</h3>
+                    </div>
+                    <div className="events__items">
+                        <div className="event-item">
+                            <div className="event-item__body">
+                                <div className="event-item__title">Young Talent's Award</div>
+                                <div className="event-item__description">Awarded for outstanding academic performance and consistent excellence.</div>
+                            </div>
+                            <div className="event-item__timespan">2020</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
--- src/assets/components/Icons.tsx
+++ src/assets/components/Icons.tsx
@@ -1,10 +1,12 @@
 import type { FunctionComponent, SVGProps } from "react";
 
 import ArrowUpRight from "../icons/arrow-up-right.svg?react";
+import AwardFill from "../icons/award-fill.svg?react";
 import ChevronDown from "../icons/chevron-down.svg?react";
 import GeoAltFill from "../icons/geo-alt-fill.svg?react";
 import PortfolioIcon from "../icons/icon.svg?react";
 import PortfolioIconLarge from "../icons/icon-large.svg?react";
+import MortarboardFill from "../icons/mortarboard-fill.svg?react";
 
 export type SvgIconProps = SVGProps<SVGSVGElement> & {
     title?: string;
@@ -18,10 +20,12 @@ export type IconMap = Record<string, SvgIcon>;
 
 const Icons = {
     ArrowUpRight,
+    AwardFill,
     ChevronDown,
     GeoAltFill,
     PortfolioIcon,
     PortfolioIconLarge,
+    MortarboardFill,
 } satisfies Record<string, SvgIcon>
 
 export default Icons;
