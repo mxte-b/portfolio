2f8babea9eab02c4c2db6d5cda85cdb6cfbda163
mxte-b <182339124+mxte-b@users.noreply.github.com>
2026-08-22 16:07:23 +0200

    fix: Fixed Safari overflow bug, adjusted navbar item order

--- src/assets/data/navbarItems.ts
+++ src/assets/data/navbarItems.ts
@@ -1,10 +1,10 @@
 import type { NavbarItem } from "../types/general";
 
 const navbarItems: NavbarItem[] = [
+    {title: "Skills", waypointId: "skills", icon: "LightningChargeFill"},
     {title: "About me", waypointId: "aboutMe", icon: "PersonFill"}, 
-    {title: "Artworks", waypointId: "artworks", icon: "ImageFill"}, 
     {title: "Projects", waypointId: "projects", icon: "BracesAsterisk"}, 
-    {title: "Skills", waypointId: "skills", icon: "LightningChargeFill"}
+    {title: "Artworks", waypointId: "artworks", icon: "ImageFill"}, 
 ];
 
 export default navbarItems;
